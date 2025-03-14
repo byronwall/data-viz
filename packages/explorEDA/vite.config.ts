2e536f1970951bb353748ad6da962ee3ac45d8a4
Byron Wall <byron@byroni.us>
2025-03-13 22:40:47 -0400

    Update base URL

--- packages/explorEDA/vite.config.ts
+++ packages/explorEDA/vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/data-viz/",
+  base: "/explorEDA/",
 
   resolve: {
     alias: {
